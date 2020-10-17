e4745f2fa500cf6facf03395de9ee9aa1320d16e
Kevin <knphillips01@gmail.com>
2020-10-17 15:06:02 -0500

    implemented auth context and initial state

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home'
 import './App.css';
 
-import FaqState from './context/faq/FaqState'
+import FaqState from './context/faq/FaqState';
+import AuthState from './context/auth/AuthState;'
 
 const App = () => {
   return (
     <>
-      <FaqState>
-        <Router>
-          <>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-              </Switch>
-            </div>
-          </>
-        </Router>
-      </FaqState>
+      <AuthState>
+        <FaqState>
+          <Router>
+            <>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </FaqState>
+      </AuthState>
     </>
   )
 }
