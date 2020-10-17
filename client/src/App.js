1e32d0df7931ab722040e134df10e6040cf6a8fb
Kevin <knphillips01@gmail.com>
2020-10-17 16:44:31 -0500

    auth register and login forms  done

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home'
+import Register from './components/auth/Register';
+import Login from './components/auth/Login'
+
 import './App.css';
 
 import FaqState from './context/faq/FaqState';
-import AuthState from './context/auth/AuthState;'
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
@@ -18,6 +21,8 @@ const App = () => {
               <div className='container'>
                 <Switch>
                   <Route exact path='/' component={Home} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
                 </Switch>
               </div>
             </>
